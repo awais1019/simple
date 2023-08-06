5ce4bd2b7ee5ac1e04d7ec1bdfc2e12688647f0c
M Awais Ashraf <mhawais154@gmail.com>
2023-08-05 17:18:35 -0700

    hello this is first lesson

---
+++ task.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+#include <fstream>
+#include <string>
+using namespace std;
+main()
+{
+    int number;
+    cout << "Enter a number:";
+    cin >> number;
+    cout << number;
+}
